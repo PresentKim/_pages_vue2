a4a925dc46fed12f5055192a4199d1d7077edf21
PresentKim <debe3721@gmail.com>
2020-03-12 23:30:00 +0900

    :art: Change color theme to nord (in dark)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      dark: {
+        primary:    "#D8DEE9", //nord4
+        info:       "#ECEFF4", //nord6
+        secondary:  "#E5E9F0", //nord5
+        accent:     "#88C0D0", //nord7
+        error:      "#BF616A", //nord11
+        warning:    "#EBCB8B", //nord13
+        success:    "#A3BE8C", //nord14
+      },
+    },
+  },
 });
