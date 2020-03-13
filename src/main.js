58b70feb15c50b23b8d4b161d7013241a8efdf75
PresentKim <debe3721@gmail.com>
2020-03-13 23:43:45 +0900

    :hammer: Move `store/index.js` to `plugins/store.js`

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './plugins/router';
-import store from './store'
+import store from './plugins/store';
 
 Vue.config.productionTip = false;
 
