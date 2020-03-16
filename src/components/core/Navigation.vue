bafb6973b9be4fcfd3f88f71bf68b8e2f60144c4
PresentKim <debe3721@gmail.com>
2020-03-16 23:44:44 +0900

    :zap: Update navigation - Add subtitles (General, Tools, Fun) - Divid items by subtitle category - Add tools/passwordstength

--- src/components/core/Navigation.vue
+++ src/components/core/Navigation.vue
@@ -1,6 +1,7 @@
 <template>
     <v-navigation-drawer v-model="drawer" app clipped temporary>
-        <v-list dense>
+        <v-list flat subheader>
+            <v-subheader>General</v-subheader>
             <v-list-item to="/">
                 <v-list-item-action>
                     <v-icon>mdi-home</v-icon>
@@ -9,6 +10,20 @@
                     <v-list-item-title>Home</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
+            <v-divider />
+            <v-spacer />
+            <v-subheader>Tools</v-subheader>
+            <v-list-item to="/tools/passwordstength">
+                <v-list-item-action>
+                    <v-icon>mdi-textbox-lock</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>Password Stength</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-spacer />
+            <v-subheader>Fun</v-subheader>
         </v-list>
         <Footer />
     </v-navigation-drawer>
