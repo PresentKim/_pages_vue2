f3891bcc8fa39109e8eef578cd66d59195ef8605
PresentKim <debe3721@gmail.com>
2020-03-19 23:32:02 +0900

    :truck: :hammer: Refactoring store

--- store/index.js
+++ store/index.js
@@ -1,21 +1,19 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {
+export const state = () => ({
     drawer: false,
     darkMode: true
-  },
-  mutations: {
+});
+
+export const mutations = {
     toggleDrawer(state) {
       state.drawer = !state.drawer;
     },
     toggleTheme(state) {
       state.darkMode = !state.darkMode;
     }
-  },
-  plugins: [createPersistedState()]
-});
+};
+
+export const plugins = [
+    createPersistedState()
+];
