16d12daccf7cb479d4150d146cf47222493bb1e4
PresentKim <debe3721@gmail.com>
2020-03-19 23:44:12 +0900

    :truck: :hammer: Refactoring App.vue to layouts/default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app>
+        <AppBar />
+        <Navigation />
+        <v-content>
+            <v-container>
+                <nuxt />
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    import AppBar from "@/components/core/AppBar.vue"
+    import Navigation from "@/components/core/Navigation.vue";
+    
+    export default {
+        components: {
+            Navigation,
+            AppBar
+        },
+    
+        created() {
+            this.$store.state.drawer = false;
+            this.$vuetify.theme.dark = this.$store.state.darkMode;
+            this.$store.subscribe((mutation, state) => this.$vuetify.theme.dark = state.darkMode);
+        }
+    }
+</script>
